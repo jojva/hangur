6ec08d05bda5a55fc361a7b0889c031dac96cd1e
Joris Valette <joris.valette@gmail.com>
2018-09-19 23:50:35 +0200

    Read first line of dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::io::{Error, ErrorKind};
+
+fn pick_a_word() -> std::io::Result<String> {
+    let file = File::open("dictionary.txt")?;
+    let content = BufReader::new(file);
+    let mut lines = content.lines();
+    let line = lines.nth(1);
+    match line {
+        Some(x) => x,
+        None => Err(Error::new(ErrorKind::Other, "Bad number of line"))
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let word = pick_a_word()?;
+    println!("Word: {}", word);
+    Ok(())
 }
