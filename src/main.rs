bb02fc9e9dc5d4e5dd44b4aa54b5d1af1d4cb328
Joris Valette <joris.valette@gmail.com>
2018-09-20 22:57:42 +0200

    Read random word of dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+use rand::{thread_rng, Rng};
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read, Seek, SeekFrom};
 use std::io::{Error, ErrorKind};
 
 fn pick_a_word() -> std::io::Result<String> {
+    let mut rng = thread_rng();
     let file = File::open("dictionary.txt")?;
-    let content = BufReader::new(file);
-    let mut lines = content.lines();
-    let line = lines.nth(1);
+    let mut content = BufReader::new(file);
+    let line_count = content.by_ref().lines().count();
+    let line_number = rng.gen_range(0, line_count);
+    content.seek(SeekFrom::Start(0))?;
+    let line = content.by_ref().lines().nth(line_number);
+    println!("Will attempt to return word {}/{}", line_number, line_count);
     match line {
-        Some(x) => x,
-        None => Err(Error::new(ErrorKind::Other, "Bad number of line"))
+        Some(word) => word,
+        None => Err(Error::new(ErrorKind::Other, "Bad number of line: {}/{}"))
     }
 }
 
